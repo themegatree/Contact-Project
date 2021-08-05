36c075bf8a877a36db39213372d16e6016694830
RyanVanDijck <rvandijck02@outlook.com>
2021-08-05 15:15:36 +0100

    Creating first test

---
+++ cypress/integration/main.spec.js
@@ -0,0 +1,14 @@
+describe('Main Tests',() => {
+    it('Adding a contact', () => {
+        cy.visit('/contacts')
+        cy.get('#nameInput').type('Mike')
+        cy.get('#emailInput').type('mike@mikemail.za')
+        cy.get('#dobInput').type('1990-09-21')
+        cy.get('#contactAdd').click() 
+
+        cy.get('#contactName0').should('contain', 'Name: Mike')
+        cy.get('#contactEmail0').should('contain', 'Email: mike@mikemail.za')
+        cy.get('#contactDob0').should('contain','Date of Birth: 1990-09-21') 
+        
+    })
+})
