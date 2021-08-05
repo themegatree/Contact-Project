33773c4502cbe81625e96d1c9bbbd96d5e6d0a0e
RyanVanDijck <rvandijck02@outlook.com>
2021-08-05 17:51:16 +0100

    Trying to edit

--- app.js
+++ app.js
@@ -26,6 +26,30 @@ app.post('/contacts', async(req, res) => {
     res.redirect('/contacts')
 })
 
+app.post("/contacts/edit/:id", async(req, res) => {
+    console.log('Post')
+    app.locals.currentID = req.params.id; 
+    res.render('edit',{
+        id: req.params.id
+    })
+})
+
+app.put("/contacts", async(req, res) => {
+    console.log("Put")
+    await models.Contacts.update(
+        {
+            name: req.body.nameInput,
+            email: req.body.emailInput,
+            dofb: req.body.dobInput
+        },{
+
+        where:{
+            id:app.locals.currentID
+        }
+    })
+    res.redirect("/contacts")
+})
+
 app.delete("/contacts/:id", async(req, res) => {
 
     await models.Contacts.destroy({
